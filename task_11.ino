e864da7fa51f517336fd090e2071d8b221ed1d5f
Omar-Shawada <138535043+Omar-Shawada@users.noreply.github.com>
2023-07-08 20:28:23 +0300

    Update task_11.ino

--- task_11.ino
+++ task_11.ino
@@ -1,40 +1,32 @@
-
-
 void setup(){
-  Serial.begin(9600);
- 
-  pinMode(9,OUTPUT);
+ Serial.begin(9600); 
+  pinMode(8,OUTPUT);
+    pinMode(9,OUTPUT);
     pinMode(10,OUTPUT);
-      pinMode(11,OUTPUT);
-digitalWrite(9,HIGH);
-delay(500);
-  digitalWrite(9,LOW);
+  
+  digitalWrite(8,HIGH);
+  delay(400);
+  
   
   for(int x=3;x>0;x--){
-    digitalWrite(10,HIGH);
+    digitalWrite(9,HIGH);
 delay(500);
-  digitalWrite(10,LOW);
-    delay(500);
-   }
-  digitalWrite(9,HIGH);
-    digitalWrite(11,HIGH);
-  delay(500);
+      digitalWrite(9,LOW);
+    delay(500);}
+       
+  
+  digitalWrite(8,LOW);
   digitalWrite(9,LOW);
-    digitalWrite(11,LOW);
-  delay(500);
 
+  
+  
+  
+  digitalWrite(10,HIGH);
+delay(500);
+  
+      digitalWrite(10,LOW);
+    delay(500);
+  
 }
-void loop(){
-
-
-
-
-
-
-
-
-
-
-
-
-}
+      
+  void loop(){}
